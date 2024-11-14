9bc422ff3607b7fd02bfd6a6556e89a708efa14d
Ionut Grecu <ionut@grecu.eu>
2024-11-14 08:52:37 +0200

    use 7za

--- backup.sh
+++ backup.sh
@@ -20,19 +20,19 @@ for path_info in "${paths[@]}"; do
     echo "Backup path: $path"
 
     if [[ "$BACKUP_TYPE" == "0" ]]; then
-        rclone copy "$path" "$RCLONE_REMOTE/"
+        rclone copy --ignore-checksum --no-check-certificate -v "$path" "$RCLONE_REMOTE/"
     elif [[ "$BACKUP_TYPE" == "1" ]]; then
-        rclone sync "$path" "$RCLONE_REMOTE/"
+        rclone sync --ignore-checksum --no-check-certificate -v "$path" "$RCLONE_REMOTE/"
     elif [[ "$BACKUP_TYPE" == "2" ]]; then
         for folder in "$path"/*; do
             folder_name=$(basename "$folder")
-            zip -r -"$COMPRESSION_LEVEL" -P "$ENCRYPTION_PASSWORD" "/tmp/${folder_name}.zip" "$folder"
-            rclone move "/tmp/${folder_name}.zip" "$RCLONE_REMOTE/"
+            7za a -t7z -mhe=on -mx="$COMPRESSION_LEVEL" -p"$ENCRYPTION_PASSWORD" "/tmp/${folder_name}.7z" "$folder"
+            rclone move --ignore-checksum --no-check-certificate -v "/tmp/${folder_name}.7z" "$RCLONE_REMOTE/"
         done
     elif [[ "$BACKUP_TYPE" == "3" ]]; then
         folder_name="$(basename "$path")_$DATE"
-        zip -r -"$COMPRESSION_LEVEL" -P "$ENCRYPTION_PASSWORD" "/tmp/$folder_name.zip" "$path"
-        rclone move "/tmp/$folder_name.zip" "$RCLONE_REMOTE/"
+        7za a -t7z -mhe=on -mx="$COMPRESSION_LEVEL" -p"$ENCRYPTION_PASSWORD" "/tmp/$folder_name.7z" "$path"
+        rclone move --ignore-checksum --no-check-certificate -v "/tmp/$folder_name.7z" "$RCLONE_REMOTE/"
     else
         echo "Invalid backup type: $BACKUP_TYPE for path: $path"
         continue
