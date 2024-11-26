b59bee54eb6eb9ad95e1d9ad80d4375788552158
Ionut Grecu <ionut@grecu.eu>
2024-11-26 07:53:18 +0200

    rsync --size-only

--- backup.sh
+++ backup.sh
@@ -21,22 +21,22 @@ for path_info in "${paths[@]}"; do
     echo "Backup path: $path"
 
     if [[ "$BACKUP_TYPE" == "0" ]]; then
-        output=$(rclone copy --ignore-checksum --no-check-certificate -v "$path" "$RCLONE_REMOTE/" 2>&1)
+        output=$(rclone copy --size-only --ignore-checksum --no-check-certificate -v "$path" "$RCLONE_REMOTE/" 2>&1)
     elif [[ "$BACKUP_TYPE" == "1" ]]; then
-        output=$(rclone sync --ignore-checksum --no-check-certificate -v "$path" "$RCLONE_REMOTE/" 2>&1)
+        output=$(rclone sync --size-only --ignore-checksum --no-check-certificate -v "$path" "$RCLONE_REMOTE/" 2>&1)
     elif [[ "$BACKUP_TYPE" == "2" ]]; then
         output=""
         
         for folder in "$path"/*; do
             folder_name=$(basename "$folder")
             7za a -t7z -mhe=on -mx="$COMPRESSION_LEVEL" -p"$ENCRYPTION_PASSWORD" "/tmp/${folder_name}.7z" "$folder"
-            output+=$(rclone move --ignore-checksum --no-check-certificate -v "/tmp/${folder_name}.7z" "$RCLONE_REMOTE/" 2>&1)
+            output+=$(rclone move --size-only --ignore-checksum --no-check-certificate -v "/tmp/${folder_name}.7z" "$RCLONE_REMOTE/" 2>&1)
             output+="<br>"
         done
     elif [[ "$BACKUP_TYPE" == "3" ]]; then
         folder_name="$(basename "$path")_$DATE"
         7za a -t7z -mhe=on -mx="$COMPRESSION_LEVEL" -p"$ENCRYPTION_PASSWORD" "/tmp/$folder_name.7z" "$path"
-        output=$(rclone move --ignore-checksum --no-check-certificate -v "/tmp/$folder_name.7z" "$RCLONE_REMOTE/" 2>&1)
+        output=$(rclone move --size-only --ignore-checksum --no-check-certificate -v "/tmp/$folder_name.7z" "$RCLONE_REMOTE/" 2>&1)
     else
         output="Invalid backup type: $BACKUP_TYPE for path: $path"
         continue
